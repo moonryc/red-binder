52bd513aa121c17d48cdb6dd4eed2b1749e4c1fe
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-24 23:23:22 -0500

    removed unneeded code

--- client/mobile/App.tsx
+++ client/mobile/App.tsx
@@ -1,20 +1,11 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { Navigation } from './navigation';
 import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
 import AllContextProvider from './context/AllContextProvider';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default function App() {
   return (
