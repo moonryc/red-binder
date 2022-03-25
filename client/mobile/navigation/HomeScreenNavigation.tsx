b17d2328096b1cdff1c041a1399b4669afbc2ba9
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-24 23:21:37 -0500

    created themes for the app/ navigators. and now the theme for the different navigators can be passed down through the HomeScreenNavigation

--- client/mobile/navigation/HomeScreenNavigation.tsx
+++ client/mobile/navigation/HomeScreenNavigation.tsx
@@ -2,54 +2,48 @@ import React from 'react';
 import { TabBindersNavigation, TabCalendarNavigation, TabSettingsNavigation, TabTodaysInfoNavigation } from './tabs';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { View } from 'react-native';
+import { useTheme } from '@react-navigation/native';
 
 const Drawer = createMaterialBottomTabNavigator();
 
 export const HomeScreenNavigation = () => {
 
-  const screensArray=[
-    {
-      name:'Calendar',
-      component:TabCalendarNavigation,
-      tabBarLabel:'Calendar',
-      tabBarIcon:'calendar',
+  const screenOptions=(name:string,icon:string)=>{
+    return {
+      tabBarIcon: ({color}:{color:any})=><MaterialCommunityIcons name={icon} color={color} size={26} />
+    };
+  };
+
+  const {colors}=useTheme();
+
+  const navigationOptionStyle = {
+    // title: 'My home',
+    headerStyle: {
+      backgroundColor: colors.primary
     },
-    {
-      name:'Today\'s info',
-      component:TabTodaysInfoNavigation,
-      tabBarLabel:'Today\'s info',
-      tabBarIcon:'note',
-    },
-    {
-      name:'Binders',
-      component:TabBindersNavigation,
-      tabBarLabel:'Binders',
-      tabBarIcon:'book',
-    },
-    {
-      name:'Settings',
-      component:TabSettingsNavigation,
-      tabBarLabel:'Settings',
-      tabBarIcon:'cog',
+    headerTintColor: colors.text,
+    headerTitleStyle: {
+      fontWeight: 'bold'
     }
-  ];
+  } as const;
+
+  const Calendar =()=> (<TabCalendarNavigation navigationOptionStyle={navigationOptionStyle}/>);
+  const Today =()=>(<TabTodaysInfoNavigation navigationOptionStyle={navigationOptionStyle}/>);
+  const Binders=()=>(<TabBindersNavigation navigationOptionStyle={navigationOptionStyle}/>);
+  const Settings=()=>(<TabSettingsNavigation navigationOptionStyle={navigationOptionStyle}/>);
 
   return (
-
-    <Drawer.Navigator initialRouteName={'Calendar'} activeColor="#f0edf6"
-      inactiveColor="#3e2465"
-      barStyle={{ backgroundColor: '#007cd4' }}>
-      {screensArray.map((screens,index)=>{
-        return (<Drawer.Screen key={index} name={screens.name} component={screens.component} options={{
-          tabBarLabel: screens.name,
-          tabBarIcon: ({color})=><MaterialCommunityIcons name={screens.tabBarIcon} color={color} size={26} />
-        }}/>);
-      })}
-
-
-    </Drawer.Navigator>
-
-
+    <View style={{flex:1}} collapsable={false}>
+      <Drawer.Navigator initialRouteName={'Calendar'} activeColor="#f0edf6"
+        inactiveColor="#3e2465"
+        barStyle={{ backgroundColor: '#007cd4' }}>
+        <Drawer.Screen name={'Calendar'} component={Calendar} options={screenOptions('Calendar','calendar')}/>
+        <Drawer.Screen name={'Today\'s Info'} component={Today} options={screenOptions('Today\'s Info','note')}/>
+        <Drawer.Screen name={'Binders'} component={Binders} options={screenOptions('Binders','book')}/>
+        <Drawer.Screen name={'Settings'} component={Settings} options={screenOptions('Settings','cog')}/>
+      </Drawer.Navigator>
+    </View>
   );
 };
 
