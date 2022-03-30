d1edf734822816e83d84542f552b4dfafcf786f1
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-29 23:15:19 -0500

    models created

--- backend/models/Account.ts
+++ backend/models/Account.ts
@@ -1,12 +1,18 @@
 import { model, Schema } from 'mongoose';
+import { hash } from 'bcrypt';
 
 
 const AccountSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true },
+  username: { type: String, required: true, unique: true, validate:[({length}:{length:number})=>length>=8,'Username must be at least 8 characters']},
+  password: { type: String, required: true, match:[/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character']},
+  email: { type: String, required: true,unique:true, match: [/.+@.+\..+/, 'Please enter a valid e-mail address']},
   users: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
 
+AccountSchema.pre('save',async function(next) {
+
+  this.password = await hash(this.password, 10);
+  return next();
+});
 
 export const Account = model('Account', AccountSchema);
--- backend/models/Dosage.ts
+++ backend/models/Dosage.ts
@@ -2,25 +2,24 @@ import { model, Schema } from 'mongoose';
 
 
 const DosageSchema = new Schema({
-  medication_name:{type:String, require:true},
-  dosage_amount:{type:Number, require: true},
-  dosage_measurement:{type:String, require:true},
-  time_to_take: { type:Date,require:true },
-  daily     : { type:Boolean,require:true },
-  weekly    : { type:Boolean,require:true },
-  monthly   : { type:Boolean,require:true },
-  monday    : { type:Boolean,require:true },
-  tuesday   : { type:Boolean,require:true },
-  wednesday : { type:Boolean,require:true},
-  thursday  : { type:Boolean,require:true},
-  friday    : { type:Boolean,require:true },
-  saturday  : { type:Boolean,require:true},
-  sunday    : { type:Boolean,require:true },
+  medication_name: { type: String, require: true },
+  dosage_amount: { type: Number, require: true },
+  dosage_measurement: { type: String, require: true },
+  time_to_take: { type: Date, require: true },
+  daily: { type: Boolean, require: true },
+  weekly: { type: Boolean, require: true },
+  monthly: { type: Boolean, require: true },
+  monday: { type: Boolean, require: true },
+  tuesday: { type: Boolean, require: true },
+  wednesday: { type: Boolean, require: true },
+  thursday: { type: Boolean, require: true },
+  friday: { type: Boolean, require: true },
+  saturday: { type: Boolean, require: true },
+  sunday: { type: Boolean, require: true }
 
 });
 
 
-
-export const Dosage = model('Dosage',DosageSchema);
+export const Dosage = model('Dosage', DosageSchema);
 
 
