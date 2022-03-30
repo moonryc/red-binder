110e41a51ca699ac09ef39e7a9f307053d08b6e9
moonryc <70920813+moonryc@users.noreply.github.com>
2022-03-29 23:08:42 -0500

    apollo setup

--- client/mobile/App.tsx
+++ client/mobile/App.tsx
@@ -5,18 +5,49 @@ import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
 import AllContextProvider from './context/AllContextProvider';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {Platform} from 'react-native';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider, createHttpLink
+} from '@apollo/client';
+import { getJWT } from './services';
+import { setContext } from '@apollo/client/link/context';
 
+
+const httpLink = createHttpLink({
+  uri: 'http://10.0.2.2:3001/graphql',
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = await getJWT();
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <AllContextProvider>
-        <TailwindProvider utilities={utilities}>
-          <StatusBar style="auto" />
-          <Navigation/>
-        </TailwindProvider>
-      </AllContextProvider>
-    </SafeAreaProvider>
+    <ApolloProvider client={client}>
+      <SafeAreaProvider>
+        <AllContextProvider>
+          <TailwindProvider utilities={utilities}>
+            <StatusBar style='auto' />
+            <Navigation />
+          </TailwindProvider>
+        </AllContextProvider>
+      </SafeAreaProvider>
+    </ApolloProvider>
   );
 }
 
