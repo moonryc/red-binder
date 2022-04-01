5b325934c795305cec5fefb768f6a6e3f1771b03
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:52:23 -0500

    updated resolvers

--- backend/schemas/resolvers.ts
+++ backend/schemas/resolvers.ts
@@ -1,19 +1,13 @@
-import { accountData, createAccount, createUser, getAllUsers, getOneUserByUserId, loginAccount } from '../controllers';
+import { createAccount, createBinder, createMedication, getAllBindersByAccountId, loginAccount } from '../controllers';
 
 export const resolvers = {
   Query: {
-    getAllUsers:getAllUsers,
-    getOneUserByUserId:getOneUserByUserId,
+    getAllBindersByAccountId:getAllBindersByAccountId,
   },
   Mutation: {
     login:loginAccount,
     createAccount:createAccount,
-    createUser:createUser,
-    // createMedication: (_: any, body:any)=>createMedication(body),
-    // createDosage: (_: any, body:any) => createDosage(body),
+    createBinder:createBinder,
+    createMedication:createMedication
   },
-
-  Subscriptions:{
-    accountData:accountData
-  }
 };
