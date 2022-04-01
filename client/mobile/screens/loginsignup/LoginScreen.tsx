cb8dda24b5b263c4fb5f67d60bd59a09218ffece
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:42:37 -0500

    made login look nicer and faster for dev by using .env

--- client/mobile/screens/loginsignup/LoginScreen.tsx
+++ client/mobile/screens/loginsignup/LoginScreen.tsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 
-import { SafeAreaView, TextInput, View } from 'react-native';
+import { ActivityIndicator, SafeAreaView, Text, View } from 'react-native';
 import { StandardButton } from '../../components/buttons/StandardButton';
-import { useMainStoreContext } from '../../context/AllContextProvider';
+import { useLoginApi } from '../../hooks/api/useLoginApi';
+import { useNavigation } from '@react-navigation/native';
+import { StandardInput } from '../../components/inputs/StandardInput';
+import { DEV_PASSWORD, DEV_USERNAME, NODE_ENV } from '@env';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { LoginStackParamList } from '../../navigation';
 
-export const LoginScreen = ({navigation}) => {
+type loginScreenProp = NativeStackNavigationProp<LoginStackParamList, 'Login'>;
 
-  const { loginApi} =useMainStoreContext();
+export const LoginScreen = () => {
 
-  const [loginBody, setLoginBody] = useState({username:'tittiestitties',password:'TittiesTitties1!'});
-  const {username,password} = loginBody;
+  const navigation = useNavigation<loginScreenProp>();
+  const { loginApi,loading,error } = useLoginApi();
+  const [loginBody, setLoginBody] = useState({ username: NODE_ENV ?DEV_USERNAME:'', password: NODE_ENV?DEV_PASSWORD:'' });
+  const { username, password } = loginBody;
 
-  const onTextChange = (text:string, value:'username'|'password') => {
-    setLoginBody({...loginBody, [value]:text});
+  const onTextChange = (text: string, value: 'username' | 'password') => {
+    setLoginBody({ ...loginBody, [value]: text });
   };
 
   const signUp = () => {
@@ -22,10 +29,13 @@ export const LoginScreen = ({navigation}) => {
   return (
     <SafeAreaView>
       <View>
-        <TextInput placeholder={'Username'} value={username} onChangeText={(text)=>onTextChange(text,'username')}/>
-        <TextInput secureTextEntry={true} placeholder={'Password'} value={password} onChangeText={(text)=>onTextChange(text,'password')}/>
-        <StandardButton onPress={()=>loginApi({variables:loginBody})} fontSize={'text-lg'} color={'red'}>Login</StandardButton>
-        <StandardButton onPress={()=>signUp()} fontSize={'text-lg'} color={'red'}>Signup</StandardButton>
+        <StandardInput fontSize={'text-lg'} placeholder={'Username'} value={username} onChangeText={(text:string) => onTextChange(text, 'username')}/>
+        <StandardInput fontSize={'text-lg'} secureTextEntry={true} placeholder={'Password'} value={password}
+          onChangeText={(text:string) => onTextChange(text, 'password')} />
+        <StandardButton disabled={loading} onPress={() => loginApi({ variables: loginBody })} fontSize={'text-lg'}
+          color={'red'}>{loading? <ActivityIndicator animating={loading} size={'large'}/>:'Login'}</StandardButton>
+        <StandardButton onPress={() => signUp()} fontSize={'text-lg'} color={'red'}>Signup</StandardButton>
+        <Text>{error ? `error : ${error}` : ''}</Text>
       </View>
     </SafeAreaView>
   );
