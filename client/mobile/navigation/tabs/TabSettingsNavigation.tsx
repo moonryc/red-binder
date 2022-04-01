f086e883b625e3a8e70f6d69d1c8578cd3ea4bab
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:33:58 -0500

    created Stack ParamList

--- client/mobile/navigation/tabs/TabSettingsNavigation.tsx
+++ client/mobile/navigation/tabs/TabSettingsNavigation.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SettingsScreen } from '../../screens';
+import { ParamListBase } from '@react-navigation/native';
 
 export const SettingsStack = createNativeStackNavigator();
+
+export interface SettingsStackParamList extends ParamListBase {
+  SettingsHome: undefined,
+}
 
 // @ts-ignore
 export const TabSettingsNavigation = ({navigationOptionStyle}) => {
 
   return (
     <View style={{flex:1}} collapsable={false} >
-      <SettingsStack.Navigator initialRouteName={'Settings Home'} screenOptions={{headerShown:true}}>
-        <SettingsStack.Screen name={'Settings Home'} component={SettingsScreen} options={{ ...navigationOptionStyle, title:'Settings' }}/>
+      <SettingsStack.Navigator initialRouteName={'SettingsHome'} screenOptions={{headerShown:true}}>
+        <SettingsStack.Screen name={'SettingsHome'} component={SettingsScreen} options={{ ...navigationOptionStyle, title:'Settings' }}/>
       </SettingsStack.Navigator>
     </View>
   );
