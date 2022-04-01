6a3498a8e8d149154b0a351db4e70dd70580d95b
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:55:34 -0500

    added interface

--- backend/controllers/medication-controller.ts
+++ backend/controllers/medication-controller.ts
@@ -1,6 +1,23 @@
 
 //@ts-ignore
-export const createMedication = async ({ name, bottle_dosage_amount, bottle_dosage_measurement, next_refill, notes }) => {
-  console.log(name);
-  return 'hello';
+import { AccountBody } from '../types';
+import { signJwtToken } from '../utils';
+
+interface CreateMedicationBody {
+  name:string
+  bottle_dosage_amount: number,
+  bottle_dosage_measurement: string,
+  next_refill: string,
+  notes?: string
+}
+
+export const createMedication = async (_:any,body:CreateMedicationBody,token:AccountBody) => {
+  try{
+    console.log(body);
+    const newToken = signJwtToken(token);
+    return {token:newToken};
+  }catch (e) {
+    console.log(e);
+  }
+
 };
