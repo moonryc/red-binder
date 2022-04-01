b0b6dcd7759a8a96f263bb2ade74949012c28170
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:52:09 -0500

    updated typeDefs

--- backend/schemas/typeDefs.ts
+++ backend/schemas/typeDefs.ts
@@ -7,83 +7,46 @@ export const typeDefs = gql`
         token:String!
     }
     
+    input imageInput{
+        name:String!
+        type:String!
+        uri:String!
+    }
+    type image{
+        name:String
+        type:String!
+        uri:String!
+    }
+    
     type Account{
         _id:ID,
         username:String,
         password:String,
         email:String,
-        users:[User]
+        binders:[Binder]
     }
     
-    type User{
+    type Binder{
         _id:ID,
         name:String,
         color:String,
-        medications:[Medication],
-        dosages:[Dosage],
-        scheduled_dosages:[ScheduledDosages]
-    }
-
-    type Medication{
-        _id:ID,
-        name:String,
-        bottle_dosage_amount:Float,
-        bottle_dosage_measurement:String,
-        next_refill:String,
-        notes:String,
-    }
+        image:image
+    },
     
-    type Dosage{
-        _id:ID,
-        medication_name:String,
-        dosage_amount:Float,
-        dosage_measurement:String,
-        time_to_take: String,
-        daily     : Boolean,
-        weekly    : Boolean,
-        monthly   : Boolean,
-        monday    : Boolean,
-        tuesday   : Boolean,
-        wednesday : Boolean,
-        thursday  : Boolean,
-        friday    : Boolean,
-        saturday  : Boolean,
-        sunday    : Boolean,
-        
-    }
-
-    type ScheduledDosages{
-        _id:ID,
-        medication_name:String,
-        dosage_amount:Float,
-        dosage_measurement: String,
-        time_to_take: String,
-        taken: Boolean,
-        missed:Boolean,
-        timeTaken:String,
-    }
-    
-    type getAllUsersResult{
-        users:[User]!,
+    type getAllBindersByAccountIdResult{
+        binders:[Binder]!,
         token:String
     }
     
-    
     type Query{
-        getAllUsers:getAllUsersResult,
-        getOneUserByUserId(id:ID!):User
+        getAllBindersByAccountId:getAllBindersByAccountIdResult,
     }
     
     type Mutation{
         login(username:String!,password:String!):jwtToken!,
         createAccount(username:String!,email:String!,password:String!):jwtToken!,
-        createUser(name:String!,color:String!): jwtToken!, 
-#        createMedication(name:String!, bottle_dosage_amount:Float!, bottle_dosage_measurement:String!, next_refill:String!, notes:String):Medication 
-#        createDosage(medication_name:String!, dosage_amount:Float!, dosage_measurement:String!, time_to_take:String!, daily:Boolean!, weekly:Boolean!, monthly:Boolean!, monday:Boolean!, tuesday:Boolean!, wednesday:Boolean!, thursday:Boolean!, friday:Boolean!, saturday:Boolean!, sunday:Boolean!):Dosage
-    }
-
-    type Subscriptions{
-        accountData:[User]!
+        createBinder(name:String!,color:String!, image:imageInput!, birthDate:String!): jwtToken!,
+        createMedication(name:String!, bottle_dosage_amount:Float!, bottle_dosage_measurement:String!, next_refill:String!, notes:String):jwtToken!
     }
 `;
 
