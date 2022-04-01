42b859141ce1611d67d90e1d2477aa7ac791a1ca
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:55:52 -0500

    user-controller to binder-controller

--- backend/controllers/index.ts
+++ backend/controllers/index.ts
@@ -2,4 +2,4 @@ export * from './account-controller';
 export * from './dosages-controller';
 export * from './medication-controller';
 export * from './scheduledDosages-controller';
-export * from './user-controller';
+export * from './binder-controller';
