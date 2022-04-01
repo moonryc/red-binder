9a230ba8bb6ca7daacee79c2cc3f64f0d86c3059
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:25:40 -0500

    BinderItem.tsx updated

--- client/mobile/components/list-items/BinderItem.tsx
+++ client/mobile/components/list-items/BinderItem.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
 import { Image, Pressable, Text, View } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { BinderStackParamList, CalendarStackParamList } from '../../navigation';
 
 interface IBinderItem {
-
-  onPress?:()=>void,
-  binderOwner:string,
+  binderOwner:{
+    image:string,
+    name:string,
+    color:string,
+    _id:string
+  },
 }
 
+type binderScreenProp = NativeStackNavigationProp<BinderStackParamList, 'SelectedBinder'>;
 
-export const BinderItem:React.FC<IBinderItem> = ({onPress=()=>null,binderOwner}) => {
-
+export const BinderItem:React.FC<IBinderItem> = ({binderOwner:{image,name,color,_id}}) => {
+  const navigation = useNavigation<binderScreenProp>();
   const [isPressed, setIsPressed] = useState<boolean>(false);
-
   const tailwind = useTailwind();
   const styles = {
     container: tailwind(`flex items-center flex-row my-2 mx-4 px-8 py-6 rounded-full ${isPressed ? 'bg-sky-400' : 'bg-sky-500'}`),
@@ -21,14 +27,19 @@ export const BinderItem:React.FC<IBinderItem> = ({onPress=()=>null,binderOwner})
   } as const ;
 
 
+
+  const onPress = () => {
+    navigation.navigate('SelectedBinder', {_id, image,name});
+  };
+
   return (
     <Pressable onPressIn={()=>setIsPressed(true)} onPressOut={()=>setIsPressed(false)} onPress={()=> onPress()}>
       <View style={styles.container}>
         <Image
           style={styles.binderIcon}
-          source={require('../../assets/icon.png')}/>
+          source={{uri:image.uri}}/>
         <Text style={styles.text} numberOfLines={1}>
-          {binderOwner}
+          {name}
         </Text>
       </View>
     </Pressable>
