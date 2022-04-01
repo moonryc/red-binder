34be836adf2864c8ce6438a601172e76abefc404
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:33:53 -0500

    created Stack ParamList

--- client/mobile/navigation/tabs/TabCalendarNavigation.tsx
+++ client/mobile/navigation/tabs/TabCalendarNavigation.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { CalendarScreen } from '../../screens';
+import { ParamListBase } from '@react-navigation/native';
 
 export const CalendarStack = createNativeStackNavigator();
+
+export interface CalendarStackParamList extends ParamListBase{
+  CalendarHome: undefined,
+}
+
 
 //@ts-ignore
 export const TabCalendarNavigation= ({navigationOptionStyle}:any) => {
 
   return (
     <View style={{flex:1}} collapsable={false}>
-      <CalendarStack.Navigator initialRouteName={'Calendar Home'}>
-        <CalendarStack.Screen name={'Calendar Home'} component={CalendarScreen} options={navigationOptionStyle}/>
+      <CalendarStack.Navigator initialRouteName={'CalendarHome'}>
+        <CalendarStack.Screen name={'CalendarHome'} component={CalendarScreen} options={navigationOptionStyle}/>
       </CalendarStack.Navigator>
     </View>
   );
