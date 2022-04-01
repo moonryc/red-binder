ee96d9e1a0e4d0bf8de04e471ff26f57841734c0
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:34:14 -0500

    created Stack ParamList

--- client/mobile/navigation/tabs/TabTodaysInfoNavigation.tsx
+++ client/mobile/navigation/tabs/TabTodaysInfoNavigation.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { TodaysInfoScreen } from '../../screens';
 import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ParamListBase } from '@react-navigation/native';
 
 export const TodaysInfoStack = createNativeStackNavigator();
+
+export interface TodaysInfoStackParamList extends ParamListBase {
+  TodayHome: undefined,
+}
 
 // @ts-ignore
 export const TabTodaysInfoNavigation = ({navigationOptionStyle}) => {
@@ -11,8 +16,8 @@ export const TabTodaysInfoNavigation = ({navigationOptionStyle}) => {
   return (
     <View style={{flex:1}} collapsable={false}>
       {/*  This view is included to fix a bug that causes the navigator to not render properly on android*/}
-      <TodaysInfoStack.Navigator initialRouteName={'Today'} screenOptions={{headerShown:true}}>
-        <TodaysInfoStack.Screen name={'Today'} component={TodaysInfoScreen} options={navigationOptionStyle}/>
+      <TodaysInfoStack.Navigator initialRouteName={'TodayHome'} screenOptions={{headerShown:true}}>
+        <TodaysInfoStack.Screen name={'TodayHome'} component={TodaysInfoScreen} options={navigationOptionStyle}/>
       </TodaysInfoStack.Navigator>
     </View>
   );
