ab6bbb812629624de58b4099d57a0589c8b1dd7c
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:49:20 -0500

    prepared navigation with nativeStackNavigationProp

--- client/mobile/screens/binders/SelectedBinderMedicalHistoryScreen.tsx
+++ client/mobile/screens/binders/SelectedBinderMedicalHistoryScreen.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+
 import { SafeAreaView, ScrollView } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { BinderStackParamList } from '../../navigation';
+import { useNavigation } from '@react-navigation/native';
+
+type binderScreenProp = NativeStackNavigationProp<BinderStackParamList, 'BinderMedicalHistory'>;
 
 export const SelectedBinderMedicalHistoryScreen = () => {
+
+  const navigation = useNavigation<binderScreenProp>();
+
   return (
     <SafeAreaView>
       <ScrollView style={{height:'100%'}}>
