de041bdf38c977c397635689e56d05a8765ed3db
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:49:49 -0500

    prepared navigation with nativeStackNavigationProp

--- client/mobile/screens/binders/SelectedBinderMedications.tsx
+++ client/mobile/screens/binders/SelectedBinderMedications.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { Button, SafeAreaView, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { BinderStackParamList } from '../../navigation';
 
-import { SafeAreaView, ScrollView } from 'react-native';
+type binderScreenProp = NativeStackNavigationProp<BinderStackParamList, 'SelectedMedication'>;
 
 export const SelectedBinderMedications = () => {
+
+  const navigation = useNavigation<binderScreenProp>();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => {
+          navigation.navigate('CreateMedication');
+        }} title="New Medication" />
+      ),
+    });
+  }, [navigation]);
+
+
   return (
     <SafeAreaView>
       <ScrollView style={{height:'100%'}}>
