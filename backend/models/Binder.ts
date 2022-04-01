90ffcb348e7ecd951dd4b8ad3779561e1bc03de2
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:53:09 -0500

    renamed User to Binder and added imageSchema

--- backend/models/Binder.ts
+++ backend/models/Binder.ts
@@ -1,15 +1,24 @@
 import { model, Schema } from 'mongoose';
 
 
-const userSchema = new Schema({
+const imageSchema = new Schema({
+  name:{type:String,required:true},
+  type:{type:String,required:true},
+  uri:{type:String,required:true}
+});
+
+
+const binderSchema = new Schema({
   name: { type: String, required: true },
   color: { type: String, required: true },
+  image: { type:imageSchema,required:true },
+  birthDate: { type:String,required:true },
   medications: [{ type: Schema.Types.ObjectId, ref: 'Medication' }],
   dosages: [{ type: Schema.Types.ObjectId, ref: 'Dosage' }],
   scheduled_dosages: [{ type: Schema.Types.ObjectId,ref: 'ScheduledDosages'}]
 });
 
 
-export const User = model('User', userSchema);
+export const Binder = model('Binder', binderSchema);
 
 
