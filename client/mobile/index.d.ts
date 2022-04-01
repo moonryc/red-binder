86448ca14a110de26c9df3c47a3eda6bea92efa6
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-01 14:44:23 -0500

    added NODE_ENV

--- client/mobile/index.d.ts
+++ client/mobile/index.d.ts
@@ -1,5 +1,6 @@
 declare module '*.png'
 declare module '@env' {
+  export const NODE_ENV:string;
   export const DEV_USERNAME: string;
   export const DEV_PASSWORD: string;
   export const SERVER_URL: string;
