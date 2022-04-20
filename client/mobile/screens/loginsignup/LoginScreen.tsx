beb544e9542ad62defdc4921a160fded65004e0a
moon <rycmoon@gmail.com>
2022-04-19 23:18:49 -0500

    updated to work with new context system with authservices and best practices

--- client/mobile/screens/loginsignup/LoginScreen.tsx
+++ client/mobile/screens/loginsignup/LoginScreen.tsx
@@ -2,20 +2,28 @@ import React, { useState } from 'react';
 
 import { ActivityIndicator, SafeAreaView, Text, View } from 'react-native';
 import { StandardButton } from '../../components/buttons/StandardButton';
-import { useLoginApi } from '../../hooks/api/useLoginApi';
+
 import { useNavigation } from '@react-navigation/native';
 import { StandardInput } from '../../components/inputs/StandardInput';
 import { DEV_PASSWORD, DEV_USERNAME, NODE_ENV } from '@env';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { LoginStackParamList } from '../../navigation';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../../utils/apis';
+import { apolloErrorHandler } from '../../utils';
+import AuthServices from '../../utils/AuthServices';
+import { useApplicationContext } from '../../context/GlobalState';
+import { TOGGLE_LOGIN } from '../../context/actions';
 
 type loginScreenProp = NativeStackNavigationProp<LoginStackParamList, 'Login'>;
 
 export const LoginScreen = () => {
 
+  const {dispatch} = useApplicationContext();
+
   const navigation = useNavigation<loginScreenProp>();
-  const { loginApi,loading,error } = useLoginApi();
-  const [loginBody, setLoginBody] = useState({ username: NODE_ENV ?DEV_USERNAME:'', password: NODE_ENV?DEV_PASSWORD:'' });
+  const [ loginApi, { loading, error } ] = useMutation(LOGIN);
+  const [loginBody, setLoginBody] = useState({ username: process.env.REACT_APP_NODE_ENV ?process.env.REACT_APP_DEV_USERNAME:'', password: process.env.REACT_APP_NODE_ENV?process.env.REACT_APP_DEV_PASSWORD:'' });
   const { username, password } = loginBody;
 
   const onTextChange = (text: string, value: 'username' | 'password') => {
@@ -26,13 +34,27 @@ export const LoginScreen = () => {
     navigation.navigate('Signup');
   };
 
+  const onSubmit = async () => {
+    try{
+      const {data} = await loginApi({ variables: loginBody });
+      console.log(data);
+      await AuthServices.saveJwtToken(data.login.token);
+      dispatch({ type:TOGGLE_LOGIN });
+    }catch (e) {
+      console.log(e);
+      if(error){
+        apolloErrorHandler(error);
+      }
+    }
+  };
+
   return (
     <SafeAreaView>
       <View>
         <StandardInput fontSize={'text-lg'} placeholder={'Username'} value={username} onChangeText={(text:string) => onTextChange(text, 'username')}/>
         <StandardInput fontSize={'text-lg'} secureTextEntry={true} placeholder={'Password'} value={password}
           onChangeText={(text:string) => onTextChange(text, 'password')} />
-        <StandardButton disabled={loading} onPress={() => loginApi({ variables: loginBody })} fontSize={'text-lg'}
+        <StandardButton disabled={loading} onPress={onSubmit} fontSize={'text-lg'}
           color={'red'}>{loading? <ActivityIndicator animating={loading} size={'large'}/>:'Login'}</StandardButton>
         <StandardButton onPress={() => signUp()} fontSize={'text-lg'} color={'red'}>Signup</StandardButton>
         <Text>{error ? `error : ${error}` : ''}</Text>
--- client/mobile/screens/loginsignup/SignupScreen.tsx
+++ client/mobile/screens/loginsignup/SignupScreen.tsx
@@ -1,38 +1,117 @@
-import React, { useState } from 'react';
-import { ActivityIndicator, SafeAreaView, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, SafeAreaView, View,Text } from 'react-native';
 import { StandardInput } from '../../components/inputs/StandardInput';
 import { StandardButton } from '../../components/buttons/StandardButton';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { LoginStackParamList } from '../../navigation';
-import { useSignup } from '../../hooks/api/useSignup';
+import { useMutation } from '@apollo/client';
+import { CREATE_ACCOUNT } from '../../utils/apis';
+import { apolloErrorHandler } from '../../utils';
+import AuthServices from '../../utils/AuthServices';
+import { useApplicationContext } from '../../context/GlobalState';
+import { TOGGLE_LOGIN } from '../../context/actions';
 
-type signupScreenProp = NativeStackNavigationProp<LoginStackParamList, 'Signup'>;
+type signupScreenProp = NativeStackNavigationProp<LoginStackParamList,
+  'Signup'>;
+
+const useVerifyPassword = (password: string, verifyPassword: string, setFormError: (value: string) => void) => {
+  useEffect(() => {
+    if (verifyPassword !== password) {
+      setFormError('Error passwords do not match');
+    } else {
+      setFormError('');
+    }
+  }, [password, verifyPassword]);
+};
+
+
 
 export const SignupScreen = () => {
 
+  const {dispatch}=useApplicationContext();
+  const [signUp,{error,loading}] = useMutation(CREATE_ACCOUNT);
+
   const navigation = useNavigation<signupScreenProp>();
-  const {signUpApi,data,loading,error}= useSignup();
 
-  const [signupBody, setSignupBody] = useState({username:'',email:'',password:'',verifyPassword:''});
-  const {username,email,password,verifyPassword} = signupBody;
+  const [signupBody, setSignupBody] = useState({
+    username: '',
+    email: '',
+    password: '',
+    verifyPassword: ''
+  });
+  const { username, email, password, verifyPassword } = signupBody;
+  const [formError, setFormError] = useState<string>('');
 
-  const onTextChange = (text: string, value: 'username' | 'password'|'verifyPassword'|'email') => {
-    setSignupBody(prevState => ({ ...prevState, [value]: text }));
+  const onTextChange = (text: string, value: 'username' | 'password' | 'verifyPassword' | 'email') => {
+    setSignupBody((prevState) => ({ ...prevState, [value]: text }));
   };
+
+  useVerifyPassword(password,verifyPassword,setFormError);
+
+  const onSubmit = async() => {
+    if (formError) {
+      return;
+    }
+
+    try{
+      const { data } = await signUp({ variables: signupBody });
+      await AuthServices.saveJwtToken(data.createAccount.token);
+      dispatch(TOGGLE_LOGIN);
+    }catch (e) {
+      if(error){
+        console.log('error');
+        apolloErrorHandler(error);
+      }
+      console.log(e);
+
+    }
+
+  };
+
 
   return (
     <SafeAreaView>
       <View>
-        <StandardInput fontSize={'text-lg'} placeholder={'Username'} value={username} onChangeText={(text:string) => onTextChange(text, 'username')}/>
-        <StandardInput fontSize={'text-lg'} placeholder={'Email'} value={password}
-          onChangeText={(text:string) => onTextChange(text, 'email')} />
-        <StandardInput fontSize={'text-lg'} secureTextEntry={true} placeholder={'Password'} value={password}
-          onChangeText={(text:string) => onTextChange(text, 'password')} />
-        <StandardInput fontSize={'text-lg'} secureTextEntry={true} placeholder={'Verify Password'} value={password}
-          onChangeText={(text:string) => onTextChange(text, 'verifyPassword')} />
-        <StandardButton disabled={loading} onPress={() => signUpApi({ variables: signupBody })} fontSize={'text-lg'}
-          color={'red'}>{loading? <ActivityIndicator animating={loading} size={'large'}/>:'Login'}</StandardButton>
+        <StandardInput
+          fontSize={'text-lg'}
+          placeholder={'Username'}
+          value={username}
+          onChangeText={(text: string) => onTextChange(text, 'username')}
+        />
+        <StandardInput
+          fontSize={'text-lg'}
+          placeholder={'Email'}
+          value={email}
+          onChangeText={(text: string) => onTextChange(text, 'email')}
+        />
+        <StandardInput
+          fontSize={'text-lg'}
+          secureTextEntry={true}
+          placeholder={'Password'}
+          value={password}
+          onChangeText={(text: string) => onTextChange(text, 'password')}
+        />
+        <StandardInput
+          fontSize={'text-lg'}
+          secureTextEntry={true}
+          placeholder={'Verify Password'}
+          value={verifyPassword}
+          onChangeText={(text: string) => onTextChange(text, 'verifyPassword')}
+        />
+        <StandardButton
+          disabled={loading}
+          onPress={() => onSubmit()}
+          fontSize={'text-lg'}
+          color={'red'}
+        >
+          {loading ? (
+            <ActivityIndicator animating={loading} size={'large'} />
+          ) : (
+            'Signup'
+          )}
+        </StandardButton>
+        <Text>{error && ('there was and error')}</Text>
       </View>
     </SafeAreaView>
   );
