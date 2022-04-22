f9088e2c370659251edaf8d4ee086d29b6cd5487
moon <rycmoon@gmail.com>
2022-04-21 23:21:57 -0500

    added why-did you rerender

---
+++ client/mobile/wdyr.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+if (process.env.REACT_APP_NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
