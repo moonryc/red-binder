31cabcdbce6eee5c46c12078f55353d8eb43ca83
moon <rycmoon@gmail.com>
2022-04-25 20:11:56 -0500

    updated themes types

--- client/mobile/types/theme.ts
+++ client/mobile/types/theme.ts
@@ -1,27 +1,36 @@
 import { Theme } from '@react-navigation/native';
 
+
+
+
+
+export interface ThemeColors {
+  primary: string,
+  primaryLight: string,
+  primaryDark: string,
+  primaryBar:string,
+  text: string,
+
+
+  secondary: string,
+  secondaryLight: string,
+  secondaryDark: string,
+
+
+
+  background: string,
+  card: string,
+  border: string,
+  notification: string,
+
+  paper:string,
+  paperShadow:string
+  calendarText:string,
+
+  refill:string
+  refillPressed:string
+}
+
 export type CustomTheme = {
-  colors: {
-    primary: string,
-    primaryLight: string,
-    primaryDark: string,
-    primaryBar:string,
-    text: string,
-
-
-    secondary: string,
-    secondaryLight: string,
-    secondaryDark: string,
-
-
-
-    background: string,
-    card: string,
-    border: string,
-    notification: string,
-
-
-    refill:string
-    refillPressed:string
-  }
+  colors: ThemeColors
 } & Theme
