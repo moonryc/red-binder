16806131e3fe18656ef1136f8120647772b1a7fe
moon <rycmoon@gmail.com>
2022-04-25 20:09:23 -0500

    removed tailwind

--- client/mobile/components/list-items/BinderItem.tsx
+++ client/mobile/components/list-items/BinderItem.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Image, Pressable, Text, View } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { BinderStackParamList, CalendarStackParamList } from '../../navigation';
 import { useApplicationContext } from '../../context/GlobalState';
 import { UPDATE_SELECTED_BINDER_INDEX } from '../../context/actions';
+import { useCustomTheme, useSimpleNavigation } from '../../hooks';
 
 interface IBinderItem {
   binderOwner:{
@@ -21,20 +22,40 @@ type binderScreenProp = NativeStackNavigationProp<BinderStackParamList, 'Selecte
 
 export const BinderItem:React.FC<IBinderItem> = ({binderOwner:{image,name,color,_id},binderIndex}) => {
   const {dispatch} = useApplicationContext();
-  const navigation = useNavigation<binderScreenProp>();
   const [isPressed, setIsPressed] = useState<boolean>(false);
-  const tailwind = useTailwind();
-  const styles = {
-    container: tailwind(`flex items-center flex-row my-2 mx-4 px-8 py-6 rounded-full ${isPressed ? 'bg-sky-400' : 'bg-sky-500'}`),
-    binderIcon: {...tailwind('rounded-full'), width:50, height:50 },
-    text: tailwind('flex-1 text-xl ml-4')
-  } as const ;
-
+  const colors = useCustomTheme();
+  const styles = useMemo(()=>({
+    container: {
+      display:'flex',
+      justifyContent:'flex-start',
+      flexDirection: 'row',
+      alignItems:'center',
+      marginTop: 10,
+      marginBottom: 10,
+      marginRight:30,
+      marginLeft:30,
+      paddingLeft:15,
+      paddingRight:15,
+      paddingTop:20,
+      paddingBottom:20,
+      borderRadius: 9999,
+      backgroundColor: isPressed ? colors.primaryLight:colors.primaryDark
+    },
+    binderIcon: {width:50, height:50, borderRadius:50/2 },
+    text: {
+      marginLeft:10,
+      color:colors.text,
+      textAlign:'center',
+    }
+  } as const),[colors.primaryDark, colors.primaryLight, colors.text, isPressed]) ;
+  const {params} = useRoute();
+  const {navigate} = useSimpleNavigation(params);
 
 
   const onPress = () => {
+    //@ts-ignore
     dispatch({type:UPDATE_SELECTED_BINDER_INDEX,value:binderIndex});
-    navigation.navigate('SelectedBinder', {_id, image,name});
+    navigate('SelectedBinder',name,{_id,image});
   };
 
   return (
--- client/mobile/components/list-items/MedicationItem.tsx
+++ client/mobile/components/list-items/MedicationItem.tsx
@@ -1,42 +1,48 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
-import { Image, Pressable, Text, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { Pressable, Text, View } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
-import { IMedication } from '../../types';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { BinderStackParamList } from '../../navigation';
+import { useCustomTheme } from '../../hooks';
 
 interface props {
-  medication:IMedication
+  medicationName:string
+  onPress?:Function
 }
 
-type medicationListScreenProp = NativeStackNavigationProp<BinderStackParamList, 'SelectedBinder'>;
 
-const MedicationItem:React.FC<props> = ({medication}) => {
 
-  const navigation = useNavigation<medicationListScreenProp>();
+const MedicationItem:React.FC<props> = ({medicationName,onPress}) => {
+
+  const colors = useCustomTheme();
   const [isPressed, setIsPressed] = useState<boolean>(false);
-  const tailwind = useTailwind();
-  const styles = {
-    container: tailwind(`flex items-center flex-row my-2 mx-4 px-8 py-6 rounded-full ${isPressed ? 'bg-sky-400' : 'bg-sky-500'}`),
-    binderIcon: {...tailwind('rounded-full'), width:50, height:50 },
-    text: tailwind('flex-1 text-xl ml-4')
-  } as const ;
-
-
-
-  const onPress = () => {
-    navigation.navigate('SelectedMedication', {medication});
-  };
-
-
+  const styles = useMemo(()=>({
+    container: {
+      display:'flex',
+      justifyContent:'center',
+      flexDirection: 'row',
+      marginTop: 10,
+      marginBottom: 10,
+      marginRight:30,
+      marginLeft:30,
+      paddingLeft:15,
+      paddingRight:15,
+      paddingTop:20,
+      paddingBottom:20,
+      borderRadius: 9999,
+      backgroundColor: isPressed ? colors.primaryLight:colors.primaryDark
+    },
+    text:{
+      display:'flex',
+      flex:1,
+      color:colors.text,
+    }
+  } as const),[colors.primaryDark, colors.primaryLight, colors.text, isPressed]) ;
 
   return (
-    <Pressable onPressIn={()=>setIsPressed(true)} onPressOut={()=>setIsPressed(false)} onPress={()=> onPress()}>
+    <Pressable onPressIn={()=>setIsPressed(true)} onPressOut={()=>setIsPressed(false)} onPress={()=> onPress?.()}>
       <View style={styles.container}>
         <Text style={styles.text} numberOfLines={1}>
-          {medication.name}
+          {medicationName}
         </Text>
       </View>
     </Pressable>
