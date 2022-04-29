9c4f9cea5f2d9b29198e4b2080ab79336372c076
moon <rycmoon@gmail.com>
2022-04-28 22:03:15 -0500

    updated binder model

--- backend/models/Binder.ts
+++ backend/models/Binder.ts
@@ -1,17 +1,9 @@
 import { model, Schema } from 'mongoose';
 
-
-const imageSchema = new Schema({
-  name:{type:String,required:true},
-  type:{type:String,required:true},
-  uri:{type:String,required:true}
-});
-
-
 const binderSchema = new Schema({
   name: { type: String, required: true },
   color: { type: String, required: true },
-  image: { type:imageSchema,required:true },
+  image: { type:String,required:false},
   birthDate: { type:String,required:true },
   medications: [{ type: Schema.Types.ObjectId, ref: 'Medication' }],
   dosages: [{ type: Schema.Types.ObjectId, ref: 'Dosage' }],
@@ -20,5 +12,6 @@ const binderSchema = new Schema({
 
 
 export const Binder = model('Binder', binderSchema);
+
 
 
