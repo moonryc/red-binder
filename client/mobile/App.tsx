b8a34f227799af006977e142a389400cee095207
moon <rycmoon@gmail.com>
2022-04-28 22:00:40 -0500

    updated packages and apollo error handling

--- client/mobile/App.tsx
+++ client/mobile/App.tsx
@@ -5,17 +5,17 @@ import { Navigation } from './navigation';
 import { TailwindProvider } from 'tailwind-rn';
 import utilities from './tailwind.json';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
-
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { SERVER_URL } from '@env';
 import { ApplicationProvider } from './context/GlobalState';
 import AuthServices from './utils/AuthServices';
+import { onError } from '@apollo/client/link/error';
 
-const httpLink = createHttpLink({
-  uri: 'http://192.168.1.4:3001/graphql',
-  // uri: SERVER_URL,
-});
+import { createUploadLink } from 'apollo-upload-client';
+// const httpLink = createHttpLink({
+//   uri: 'http://192.168.1.4:3001/graphql',
+//   // uri: SERVER_URL,
+// });
 
 const authLink = setContext(async (_, { headers }) => {
   // get the authentication token from local storage if it exists
@@ -29,8 +29,34 @@ const authLink = setContext(async (_, { headers }) => {
   };
 });
 
+// const uploadLink = createUploadLink({uri:'http://192.168.1.4:3001/graphql'});
+const uploadLink = createUploadLink({uri:'http://192.168.1.4:3001/graphql'});
+// const errorLink = onError(({ graphQLErrors }) => {
+//   if (graphQLErrors) {
+//     graphQLErrors.map(({ message }) => console.log(message));
+//   }
+// });
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  }
+
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+
+  // link: authLink.concat(uploadLink),
+  // link: authLink.concat(httpLink),
+  // @ts-ignore
+  link: authLink.concat(errorLink).concat(uploadLink),
   cache: new InMemoryCache()
 });
 
