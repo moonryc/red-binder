f9572e47f560ece1a3fa6b58893bd67b1965e208
moon <rycmoon@gmail.com>
2022-04-29 20:09:49 -0500

    began working on loading screen

--- client/mobile/screens/LoadingScreen.tsx
+++ client/mobile/screens/LoadingScreen.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { LoadingProcessStatus } from '../types';
 
-const LoadingScreen = () => {
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+
+});
+
+
+interface props {
+  loginStatus:LoadingProcessStatus|string
+  loadingUserDataStatus:LoadingProcessStatus|string
+}
+
+const LoadingScreen:React.FC<props> = ({loginStatus,loadingUserDataStatus}) => {
   return (
-    <Text>
-      LOADING
-    </Text>
+    <View style={styles.container}>
+      <Text>Red-Binder</Text>
+      <ActivityIndicator color={'red'} animating={true} size={'large'} />
+      {loginStatus === LoadingProcessStatus.Loading && <Text>For Existing UserData</Text>}
+      {loadingUserDataStatus === LoadingProcessStatus.Loading && <Text>Loading Data and checking for updates</Text>}
+    </View>
   );
 };
+
 
 export default LoadingScreen;
