e1276f5eaa815bfe541da7da5acc9779c133e599
moon <rycmoon@gmail.com>
2022-04-29 20:10:31 -0500

    updated types

--- client/mobile/types/index.ts
+++ client/mobile/types/index.ts
@@ -1,3 +1,14 @@
 export * from './medications';
 export * from './binders';
 export * from './theme';
+
+export interface LoadingProcess{
+  name:string,
+  isReady:boolean
+}
+
+export enum LoadingProcessStatus{
+  Waiting='WAITING',
+  Loading='LOADING',
+  isReady='READY'
+}
