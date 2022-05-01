18111f4b8854ea15957d25dfddba35344075da72
moon <rycmoon@gmail.com>
2022-05-01 17:54:30 -0500

    made this component more reusable so that it can edit medications as well as create

--- client/mobile/screens/medications/MedicationCreateEditScreen.tsx
+++ client/mobile/screens/medications/MedicationCreateEditScreen.tsx
@@ -1,57 +1,36 @@
-import React, { useCallback, useMemo, useReducer, useState } from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import {
-  ActivityIndicator,
-  NativeSyntheticEvent,
-  NativeTouchEvent, Pressable,
-  SafeAreaView,
-  ScrollView,
-  Text, TextInput,
-  View
-} from 'react-native';
+import React, { useCallback, useReducer } from 'react';
+import { NativeSyntheticEvent, NativeTouchEvent, Platform, Pressable, Text, TextInput, View } from 'react-native';
 import { StandardInput } from '../../components/inputs/StandardInput';
-import { Picker } from '@react-native-picker/picker';
 import { StandardButton } from '../../components/buttons/StandardButton';
-import { format } from 'date-fns';
-import { useTailwind } from 'tailwind-rn';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { BinderStackParamList } from '../../navigation';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
 import { IMedication } from '../../types';
 import { useMutation } from '@apollo/client';
-import { CREATE_MEDICATION, GET_ALL_BINDERS } from '../../utils/apis';
+import { CREATE_MEDICATION, DELETE_MEDICATION, GET_ALL_BINDERS, UPDATE_MEDICATION } from '../../utils/apis';
 import { apolloErrorHandler } from '../../utils';
 import { useApplicationContext } from '../../context/GlobalState';
 import CustomScrollableView from '../../components/misc/CustomScrollableView';
 import { useCustomTheme, useSimpleNavigation } from '../../hooks';
 import { useMedicationCreateEditScreenStyles } from '../screen-styles/medications/useMedicationCreateEditScreenStyles';
 import CustomPicker from '../../components/custom-picker/CustomPicker';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import CustomPickerItem from '../../components/custom-picker/CustomPickerItem';
+import AndroidDatePicker from '../../components/custom-date-picker/AndroidDatePicker';
+import IosDatePicker from '../../components/custom-date-picker/IosDatePicker';
 
-type binderScreenProp = NativeStackNavigationProp<BinderStackParamList, 'CreateBinder'>;
 
 const dosageTypes = ['mg', 'tablet', 'drops', 'grams', 'milliLiter', 'Liter'];
 
 interface IinitialState extends IMedication {
-  isPickerOpen:boolean
+  isPickerOpen:boolean,
+  isDatePickerOpen:boolean
 }
 
-const initialState: IinitialState = {
-  name: '',
-  bottle_dosage_amount: '',
-  bottle_dosage_measurement: dosageTypes[0],
-  next_refill: new Date(),
-  notes: '',
-  isPickerOpen:false
-};
 
 interface reducerAction {
   type:'name'| 'bottleDosageAmount'|
 'bottleDosageMeasurement'|
 'nextRefill'|
 'notes'|
-'togglePicker',
+'togglePicker'| 'toggleDatePicker',
   value?: string|Date
 }
 
@@ -71,6 +50,8 @@ const reducer = (state: IinitialState, {type,value}: reducerAction): IinitialSta
     return { ...state, notes: value as string };
   case 'togglePicker':
     return { ...state, isPickerOpen: !state.isPickerOpen };
+  case 'toggleDatePicker':
+    return { ...state, isDatePickerOpen: !state.isDatePickerOpen };
   default:
     throw state;
   }
@@ -82,26 +63,42 @@ export const MedicationCreateEditScreen = () => {
   const {state:{binders,selectedBinderIndex}} = useApplicationContext();
   const colors = useCustomTheme();
   const styles = useMedicationCreateEditScreenStyles(colors);
-  const {params} = useRoute();
+  const {params}:any = useRoute();
+  let initialState: IinitialState = {
+    name: '',
+    bottle_dosage_amount: '',
+    bottle_dosage_measurement: dosageTypes[0],
+    next_refill: new Date(),
+    notes: '',
+    isPickerOpen:false,
+    isDatePickerOpen:false
+  };
+  let edit = false;
+
+  if(params?.medication){
+    initialState = {
+      ...initialState,
+      ...params.medication,
+      bottle_dosage_amount: JSON.stringify(params.medication.bottle_dosage_amount),
+      next_refill: new Date(params.medication.next_refill),
+      medicationId:params.id};
+    edit = true;
+  }
+
   const {navigate}= useSimpleNavigation(params);
 
-  const [ createMedicationApi, {loading, error }] = useMutation(CREATE_MEDICATION,{
+  const [ createMedicationApi, {loading:createLoading, error:createError }] = useMutation(CREATE_MEDICATION,{
     refetchQueries:[GET_ALL_BINDERS]
   });
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { name, bottle_dosage_amount, bottle_dosage_measurement, next_refill, notes,isPickerOpen } = state;
-  const [show, setShow] = useState(false);
+  const [updateMedication,{error:updateError,loading:updateLoading}] = useMutation(UPDATE_MEDICATION,{
+    refetchQueries:[GET_ALL_BINDERS]
+  });
+  const [deleteMedication,{error:deleteError,loading:deleteLoading}] = useMutation(DELETE_MEDICATION,{
+    refetchQueries:[GET_ALL_BINDERS]
+  });
 
-  const onChange = (_:any,selectedDate:Date|undefined|null):void => {
-    setShow(false);
-    if(selectedDate){
-      dispatch({ type: 'nextRefill', value: selectedDate });
-    }
-  };
-  const showDatePicker = useCallback((event: NativeSyntheticEvent<NativeTouchEvent>) => {
-    event.preventDefault();
-    setShow(true);
-  },[]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { name, bottle_dosage_amount, bottle_dosage_measurement, next_refill, notes,isPickerOpen,isDatePickerOpen } = state;
 
   const handleCreateMedication = useCallback(async () => {
     try{
@@ -118,24 +115,68 @@ export const MedicationCreateEditScreen = () => {
       });
       navigate('BinderMedications','Medications');
     }catch (e) {
-      if(error){
+      if(createError){
         console.log('error');
-        apolloErrorHandler(error);
+        apolloErrorHandler(createError);
       }
       console.log(e);
     }
-  }, [binders, bottle_dosage_amount, createMedicationApi, error, navigate, selectedBinderIndex, state]);
+  }, [binders, bottle_dosage_amount, createError, createMedicationApi, navigate, selectedBinderIndex, state]);
+  const handleUpdateMedication = useCallback(async () => {
+    try{
+      const {data}= await updateMedication({
+        variables: {
+          ...state,
+          bottle_dosage_amount: parseFloat(state.bottle_dosage_amount as string),
+          medicationId: params.id
+        }
+      });
+      navigate('BinderMedications','Medications');
+    }catch (e) {
+      if(updateError){
+        console.log('error');
+        apolloErrorHandler(updateError);
+      }
+      console.log(e);
+    }
+  }, [updateError, navigate, params.id, state, updateMedication]);
+  const handleDeleteMedication = useCallback(async () => {
+    try{
+      const {data}= await deleteMedication({
+        variables: {
+          medicationId: params.id
+        }
+      });
+      navigate('BinderMedications','Medications');
+    }catch (e) {
+      if(deleteError){
+        console.log('error');
+        apolloErrorHandler(deleteError);
+      }
+      console.log(e);
+    }
+  }, [deleteMedication, params.id, navigate, deleteError]);
 
+  const dateUpdate = useCallback((_:any,selectedDate:Date|undefined|null):void => {
+    dispatch({type:'toggleDatePicker'});
+    if(selectedDate){
+      dispatch({ type: 'nextRefill', value: selectedDate });
+    }
+  },[]);
+  const showDatePicker = useCallback((event: NativeSyntheticEvent<NativeTouchEvent>) => {
+    dispatch({type:'toggleDatePicker'});
+  },[]);
   const handleTogglePicker = useCallback(
     () => {
       dispatch({type:'togglePicker'});
     },[]
   );
-
   const handlePickerChange = useCallback((value:string) => {
     dispatch({type:'bottleDosageMeasurement',value});
   }, []);
-
+  const updateIosBirthdate = useCallback((_:any, selectedDate:Date) => {
+    dispatch({type:'nextRefill',value: selectedDate});
+  },[]);
 
   return (
     <CustomScrollableView>
@@ -148,22 +189,29 @@ export const MedicationCreateEditScreen = () => {
           />
           <Pressable style={styles.pressable} onPress={()=>handleTogglePicker()}>
             <Text style={styles.dosageMeasurement}>{bottle_dosage_measurement}</Text>
-            <MaterialCommunityIcons name={'menu-down'} color={colors.text} size={26}/>
           </Pressable>
         </View>
       </View>
-      <StandardInput onPress={showDatePicker} value={`Next Refill: ${format(next_refill as Date, 'dd/MM/yyyy')}`}/>
-      <StandardInput multiline={true} placeholder={'Notes'} value={notes}
+      {Platform.OS === 'android' && <AndroidDatePicker
+        isDatePickerOpen={isDatePickerOpen}
+        dateValue={next_refill as Date}
+        toggleDatePicker={showDatePicker}
+        updateDate={dateUpdate}/>}
+      {Platform.OS === 'ios' && <IosDatePicker updateDate={updateIosBirthdate} initialDate={next_refill as Date}/>}
+      <StandardInput multiline={true} placeholder={'Notes'} value={notes as string}
         onChangeText={(text: string) => dispatch({ type:'notes', value: text })} />
-      <StandardButton disabled={loading}
+      {!edit &&(<StandardButton disabled={createLoading} loading={createLoading}
         onPress={handleCreateMedication}>
-        {loading ? (<ActivityIndicator animating={true} color='#ff0000' size={'large'} />) : 'ADD MEDICATION'}
-      </StandardButton>
-      {error && (<Text style={{ textAlign: 'center' }}>{error.message}</Text>)}
-      {show && (<DateTimePicker value={next_refill as Date}
-        mode={'date'}
-        is24Hour={true}
-        onChange={onChange} />)}
+        ADD MEDICATION
+      </StandardButton>)}
+      {edit &&(<>
+        <StandardButton disabled={updateLoading} loading={updateLoading} onPress={handleUpdateMedication}>UPDATE MEDICATION</StandardButton>
+        <StandardButton disabled={updateLoading} loading={updateLoading} onPress={handleDeleteMedication}>DELETE</StandardButton>
+      </>)
+      }
+      {createError && (<Text style={{ textAlign: 'center' }}>{createError.message}</Text>)}
+      {updateError && (<Text style={{ textAlign: 'center' }}>{updateError.message}</Text>)}
+      {deleteError && (<Text style={{ textAlign: 'center' }}>{deleteError.message}</Text>)}
       <CustomPicker isPickerOpen={isPickerOpen} closeCustomPicker={handleTogglePicker} onSelectValue={handlePickerChange} title={'Dosage Type'}>
         {dosageTypes.map((color, index) => (
           <CustomPickerItem key={index} value={color}/>
