f065eb9a7b0328eaaefada1db56772e9760ff07c
moon <rycmoon@gmail.com>
2022-05-01 17:52:21 -0500

    removed tailwind and commented out things that are not within the scope of work

--- client/mobile/screens/binders/SelectedBinderScreen.tsx
+++ client/mobile/screens/binders/SelectedBinderScreen.tsx
@@ -8,6 +8,7 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { BinderStackParamList } from '../../navigation';
 import CustomScrollableView from '../../components/misc/CustomScrollableView';
 import { useSimpleNavigation } from '../../hooks';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type binderScreenProp = NativeStackNavigationProp<BinderStackParamList, 'SelectedBinder'>;
 
@@ -16,39 +17,55 @@ export const SelectedBinderScreen = () => {
 
   const {params} =useRoute();
   //@ts-ignore
-  const { image,_id} = params;
+  const { image,_id,color} = params;
   const { navigate } = useSimpleNavigation(params);
 
   const tailwind = useTailwind();
   const styles = useMemo(()=>({
-    binderContainer: tailwind('flex items-center'),
-    binderIcon: {...tailwind('flex rounded-full justify-center'), width:100, height:100 },
+    binderContainer: {
+      display:'flex',
+      flex:1,
+
+      alignItems:'center'
+    },
+    buttonsContainer:{
+      justifyContent:'space-between',
+    },
+    binderIcon: {...tailwind('flex rounded-full justify-center'), width:100, height:100, borderRadius:100/2 },
   } as const),[tailwind] );
 
+  //TODO: Not Within Scope of work currently
   const goToMedicalHistory =()=> navigate('BinderMedicalHistory');
   const goToMedications = ()=>navigate('BinderMedications');
+  //TODO: Not Within Scope of work currently
   const goToMedicalInteractions = ()=>navigate('BinderInteractions');
   const goToEditBinder = ()=>navigate('EditBinder');
 
   return (
     <CustomScrollableView>
       <View style={styles.binderContainer}>
-        <Image
+        {image ===null && <MaterialCommunityIcons name={'account-circle-outline'} color={color} size={100}/>}
+        {image !== null && <Image
           style={styles.binderIcon}
-          source={{uri:image ?`data:image/png;base64,${image}`:fallbackImage}}/>
+          source={{uri:image?`data:image/png;base64,${image}`:fallbackImage}}/>
+        }
       </View>
-      <StandardButton onPress={goToMedicalHistory}>
-          Medical History
-      </StandardButton>
-      <StandardButton onPress={goToMedications}>
+      <View style={styles.buttonsContainer}>
+        {/*TODO: Not Within Scope of work currently*/}
+        {/*<StandardButton onPress={goToMedicalHistory}>*/}
+        {/*    Medical History*/}
+        {/*</StandardButton>*/}
+        <StandardButton onPress={goToMedications}>
         Medications
-      </StandardButton>
-      <StandardButton onPress={goToMedicalInteractions}>
-        Medical Interactions
-      </StandardButton>
-      <StandardButton onPress={goToEditBinder}>
+        </StandardButton>
+        {/*TODO: Not Within Scope of work currently*/}
+        {/*<StandardButton onPress={goToMedicalInteractions}>*/}
+        {/*  Medical Interactions*/}
+        {/*</StandardButton>*/}
+        <StandardButton onPress={goToEditBinder}>
         Edit Binder
-      </StandardButton>
+        </StandardButton>
+      </View>
 
     </CustomScrollableView>
   );
