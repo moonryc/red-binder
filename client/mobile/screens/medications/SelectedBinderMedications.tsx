71ac18c198b60a144ce6307d3431af25a18cd4e4
moon <rycmoon@gmail.com>
2022-05-01 17:55:25 -0500

    fixed a bug that resulted in the add button disapearing and also made it more obvious what was being rendered

--- client/mobile/screens/medications/SelectedBinderMedications.tsx
+++ client/mobile/screens/medications/SelectedBinderMedications.tsx
@@ -1,8 +1,4 @@
-import React, { useEffect, useLayoutEffect } from 'react';
-import { Button, Text,SafeAreaView, ScrollView } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { BinderStackParamList } from '../../navigation';
+import React, { useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import { useApplicationContext } from '../../context/GlobalState';
 import { IMedication } from '../../types';
 import MedicationItem from '../../components/list-items/MedicationItem';
@@ -10,28 +6,32 @@ import CustomScrollableView from '../../components/misc/CustomScrollableView';
 import { useCustomTheme, useSimpleNavigation } from '../../hooks';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-type binderScreenProp = NativeStackNavigationProp<BinderStackParamList, 'BinderMedications'>;
 
 export const SelectedBinderMedications = () => {
 
-  const {state:{binders,selectedBinderIndex}}= useApplicationContext();
-  // const navigation = useNavigation<binderScreenProp>();
-
-
   const colors = useCustomTheme();
-  const {params} = useRoute();
-  const {navigate} = useSimpleNavigation({name:'Medications'}, ()=>(<MaterialCommunityIcons name={'plus-circle-outline'} color={colors.primaryDark} size={26} onPress={()=>navigate('CreateMedication','New Medication')}/>));
+  const {navigate,navigation} = useSimpleNavigation({name:'Medications'});
+  const {state:{binders,selectedBinderIndex}}= useApplicationContext();
+  const [medicationArray, setMedicationArray] = useState(binders![selectedBinderIndex].medications);
 
-  const hasMedications = binders && binders[selectedBinderIndex] && binders[selectedBinderIndex].medications.length > 0;
-
-  const onPress = (medication:IMedication) => {
+  const onPress = useCallback((medication:IMedication) => {
     const selectedMedication = {...medication,next_refill:medication.next_refill.toString()};
-    navigate('SelectedMedication', medication.name,{medication:selectedMedication});
-  };
+    navigate('CreateMedication', medication.name,{medication:selectedMedication,id:medication._id});
+  },[navigate]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ()=>(<MaterialCommunityIcons name={'plus-circle-outline'} color={colors.primaryDark} size={26} onPress={()=>navigate('CreateMedication','New Medication')}/>),
+    });
+  }, [colors.primaryDark, navigate, navigation,binders,medicationArray]);
+
+  useEffect(() => {
+    setMedicationArray(binders![selectedBinderIndex].medications);
+  }, [binders, selectedBinderIndex]);
 
   return (
     <CustomScrollableView>
-      {hasMedications && binders[selectedBinderIndex].medications.map((medication:IMedication,index)=>(
+      {medicationArray.map((medication:IMedication,index)=>(
         <MedicationItem key={medication._id} medicationName={medication.name} onPress={()=>onPress(medication)}/>
       ))}
     </CustomScrollableView>
